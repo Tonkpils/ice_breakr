2504f9a7b8d8c28ea4b75b469ea3866ca1fffa1d
dasmall <trucool@gmail.com>
2014-01-14 22:41:24 -0500

    Changed some hash nesting around send_data helper wont allow modification of message hash calling to_json on message in send_data helper doesn't work

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -5,12 +5,13 @@ class NotificationsController < ApplicationController
     hijack do |tubesock|
       redis_thread = Thread.new do
 
+        message = build_event "zoneuser_add"
+        message = message.to_json
+        Redis.new.publish session[:chat_zone_id], message
+
         # Subscribe each new connection to their chat zone.
         Redis.new.subscribe session[:chat_zone_id] do |on|
           message_event_block(tubesock, on)
-          
-          message = build_event "zoneuser_add"
-          Redis.new.publish(session[:chat_zone_id], message)
         end
 
         # Subscribe each user to their own notification connection
@@ -21,7 +22,7 @@ class NotificationsController < ApplicationController
       end
 
       tubesock.onopen do
-        # Redis.new.publish session[:chat_zone_id], "You are now chatting with someone..."
+
       end
       tubesock.onmessage do |m|
         Redis.new.publish session[:chat_zone_id], m
@@ -29,6 +30,7 @@ class NotificationsController < ApplicationController
 
       tubesock.onclose do
         message = build_event "zoneuser_remove"
+        message = message.to_json
         Redis.new.publish(session[:chat_zone_id], message)
         redis_thread.kill
       end
@@ -43,17 +45,16 @@ class NotificationsController < ApplicationController
     end
 
     def send_data(tubesock, channel, message)
-      client_data = { channel: channel, message: message }
-      tubesock.send_data(client_data)
+      tubesock.send_data(message)
     end
 
     # Helper to create boilerplate data for an event to be emitted.
     def build_event event
-      ({ event: event, data: { user: get_user } })
+      ({ :event => event}.merge(get_user))
     end
 
     # Helper method to get current user data.
     def get_user
-      user = { id: session[:user_id], username: session[:username] }
+      user = { :id => session[:user_id], :event_user => session[:username] }
     end
 end
